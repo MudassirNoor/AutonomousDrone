1cfb402c0885b4ee45c82eec78916c820f5a53c5
Danial <danialkha4@outook.com>
2021-04-01 19:17:16 -0700

    init commit 2

--- drone.py
+++ drone.py
@@ -1,36 +1,32 @@
-'''
-@Team Guardian Autonomy Team
-Spring 2021 Drone
-Computer Vision 
-v1 
-'''
+# Team Guardian Autonomy Team
+# Spring 2021 Drone
+# Computer Vision
 
 import numpy
 import cv2
 
 
 def capture(stream):
-	while(True):
+    while True:
 
-		# Capture frame
-		ret, frame = stream.read()
+        # Capture frame
+        ret, frame = stream.read()
 
-		# display
-		cv2.imshow('frame',frame)
+        # display
+        cv2.imshow('frame', frame)
 
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-	# free the stream and window
-	stream.release()
-	cv2.destroyAllWindows()
+    # free the stream and window
+    stream.release()
+    cv2.destroyAllWindows()
 
 
 def main():
-	# retreive video steam, 0 for webcam ID
-	capture( cv2.VideoCapture(0) )
+    # retrieve video steam, 0 for webcam ID
+    capture(stream=cv2.VideoCapture(0))
 
 
 if __name__ == "__main__":
-	main()	
-
+    main()
